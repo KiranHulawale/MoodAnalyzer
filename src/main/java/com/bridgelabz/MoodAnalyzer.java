097a3c59bce8f24c4158acc783e3e28379779da8
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-04-08 12:06:53 +0530

    Update MoodAnalyzer.java

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -3,16 +3,22 @@ package com.bridgelabz;
 public class MoodAnalyzer {
     String message;
 
-    public MoodAnalyzer(String message) {
+    public MoodAnalyzer() {
         this.message = message;
     }
 
-    public String analyseMood(){
-        if (message.contains("Sad")){
+    public String analyseMood(String message) {
+        String words[] = message.split(" ");
+        if (words[3].equalsIgnoreCase("sad"))
             return "SAD";
-        }else{
-            return "HAPPY";
-        }
+        return "HAPPY";
     }
+    
+     public static void main(String[] args) {
+        MoodAnalyzer mood = new MoodAnalyzer();
+        System.out.println(mood.analyseMood("I am in sad mood ") );
     }
+}
+
+    
 
