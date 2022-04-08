93727da1fe5ea367f22928a92f73f4605dcd628a
Kiran <kiranhulawale123@gmail.com>
2022-04-08 12:18:58 +0530

    Added TC-2.1 to return happy

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -3,14 +3,19 @@ package com.bridgelabz;
 public class MoodAnalyzer {
     String message;
 
-    public MoodAnalyzer() {
+    public MoodAnalyzer(Object o) {
+        super();
         this.message = message;
     }
 
-    public String analyseMood(String message) {
-        String words[] = message.split(" ");
-        if (words[3].equalsIgnoreCase("happy"))
-            return "SAD";
-        return "HAPPY";
+    public String analyseMood() {
+        try {
+            String words[] = message.split(" ");
+            if (words[3].equalsIgnoreCase("happy"))
+                return "SAD";
+            return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,14 +4,14 @@ import org.junit.Assert;
 class MoodAnalyzerTest {
 
     void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("I am in sad mood ");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood = moodAnalyzer.analyseMood();
         Assert.assertEquals("SAD",mood);
     }
 
     void givenMessage_WhenHappy_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood  = moodAnalyzer.analyseMood("I am in sad mood ");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood  = moodAnalyzer.analyseMood();
         Assert.assertEquals("Happy",mood);
     }
 }
