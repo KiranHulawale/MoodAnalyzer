19ed8b4a438bf4779de00922eabe83f59649568c
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-04-08 12:42:15 +0530

    Update MoodAnalyzerTest.java

--- src/main/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/main/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class MoodAnalyzerTest {
     public static void main(String[] args) {
-        MoodAnalyzer mood = new MoodAnalyzer(null);
+        MoodAnalyzer mood = new MoodAnalyzer();
         System.out.println(mood.analyseMood());
     }
 }
